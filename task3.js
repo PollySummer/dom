6d95dbb1a661ee2dc12032b4616d5521dd6df8ec
PollySummer <polinaivanenko.cat@gmail.com>
2023-08-14 12:41:24 +0300

    new

--- task3.js
+++ task3.js
@@ -1,19 +1,53 @@
-const receive = document.querySelector('.insert-text');
+const addField = document.querySelector('.insert-text');
 const list = document.querySelector('ul');
 const addBtn = document.querySelector('.add-btn');
-const deleteBtn = document.querySelector('.delete-btn');
+const deleteBtn = document.querySelector('.all-delete-btn');
+const listItems = document.querySelectorAll('li');
 
-function addNewTask() {
-    const newTask = document.createElement('li');
-    newTask.innerText = receive.value;
-    list.append(newTask);
-}
-addBtn.addEventListener('click', addNewTask);
-
-function deleteTask() {
-    let lis = document.querySelectorAll('li');
-    for (let i = 0; li = lis[i]; i++) {
-        li.parentNode.removeChild(li);
+function createDeleteBtn() {
+    for (let i = 0; i < listItems.length; i++) {
+        const delBtn = document.createElement('button');
+        delBtn.className = 'delete-button';
+        delBtn.innerText = 'delete';
+        listItems[i].appendChild(delBtn);
     }
 }
-deleteBtn.addEventListener('click', deleteTask);
+function deleteItem() {
+    createDeleteBtn();
+    const del = document.querySelectorAll('.delete-button');
+    for (let i = 0; i < del.length; i++) {
+        del[i].addEventListener('click', function () {
+            const elemet = this.parentElement;
+            elemet.style.display = "none";
+        });
+    }
+}
+deleteItem();
+
+function addItem() {
+    const newLi = document.createElement('li');
+    newLi.innerText = addField.value;
+    list.append(newLi);
+    addField.value = '';
+
+    const delBtn = document.createElement('button');
+    delBtn.className = 'delete-button';
+    delBtn.innerText = 'delete';
+    newLi.appendChild(delBtn);
+
+    const del = document.querySelectorAll('.delete-button');
+    for (let i = 0; i < del.length; i++) {
+        del[i].addEventListener('click', function () {
+            const elemet = this.parentElement;
+            elemet.style.display = "none";
+        });
+    }
+}
+addBtn.addEventListener('click', addItem);
+
+function hideAll() {
+    for (let i = 0; i < listItems.length; i++) {
+        listItems[i].style.display = 'none';
+    }
+}
+deleteBtn.addEventListener('click', hideAll);
